83fdc1a027cc82dba22656f7d305ca84e6f664ed
hoangvi2603 <106788305+hoangvi2603@users.noreply.github.com>
2022-06-03 08:48:07 +0700

    Update myJs.js

--- js/myJs.js
+++ js/myJs.js
@@ -16,7 +16,7 @@ function firstQuestion(){
     Swal.fire({
         title: 'He luu cậu!',
         text: 'Tớ có điều này muốn hỏi cậu nhớ phải trả lời thật lòng nhaaa.',
-        imageUrl: '/img/cuteCat.jpg',
+        imageUrl: 'img/cuteCat.jpg',
         imageWidth: 300,
         imageHeight: 300,
         background: '#fff url("../img/iput-bg.jpg")',
